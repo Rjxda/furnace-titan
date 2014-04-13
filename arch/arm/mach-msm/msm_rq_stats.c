afb62296e0c44c4374495d1d00e928ee13514c74
myfluxi <linflux@arcor.de>
2014-04-13 21:33:53 +0200

    msm: hotplug: Consider load instead of frequency for slowest cpu

--- arch/arm/mach-msm/msm_rq_stats.c
+++ arch/arm/mach-msm/msm_rq_stats.c
@@ -43,6 +43,7 @@ struct cpu_load_data {
 	u64 prev_cpu_idle;
 	u64 prev_cpu_wall;
 	unsigned int avg_load_maxfreq;
+	unsigned int cur_load_maxfreq;
 	unsigned int samples;
 	unsigned int window_size;
 	unsigned int cur_freq;
@@ -148,10 +149,19 @@ unsigned int report_load_at_max_freq(void)
 		mutex_lock(&pcpu->cpu_load_mutex);
 		update_average_load(pcpu->cur_freq, cpu);
 		total_load += pcpu->avg_load_maxfreq;
+		pcpu->cur_load_maxfreq = pcpu->avg_load_maxfreq;
 		pcpu->avg_load_maxfreq = 0;
 		mutex_unlock(&pcpu->cpu_load_mutex);
 	}
 	return total_load;
+}
+
+
+unsigned int report_avg_load_cpu(unsigned int cpu)
+{
+	struct cpu_load_data *pcpu= &per_cpu(cpuload, cpu);
+
+	return pcpu->cur_load_maxfreq;
 }
 
 static int cpufreq_transition_handler(struct notifier_block *nb,
