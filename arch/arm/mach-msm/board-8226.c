5af168e1b63bc086427ed723018990d170e705e5
savoca <adeddo27@gmail.com>
2014-10-28 12:11:32 +0000

    LCD_KCAL: Color control driver for mdss_mdp devices
    /sys/devices/platform/kcal_ctrl.0/kcal
    /sys/devices/platform/kcal_ctrl.0/kcal_min
    
    kcal:
    Write RGB LUT data from sysfs. Default values are '255/255/255', send
    RGB info 'x x x' to kcal to apply the value and update the LUT.
    
    kcal_min:
    Lower values such as 0-10 ultimately create a dark, washed out display,
    if a user accidently sets their kcal values to "0 0 0" they will loose
    visual control of the device, potentially requiring a hard reboot.
    Introduce minimum kcal values to prevent this behavior. kcal_min will
    detect and block values attempting to set lower than itself, resetting
    the specific values to that of kcal_min. There is also behavior to
    adjust the kcal if kcal_min is set to a level higher than that of any
    kcal value(s).

--- arch/arm/mach-msm/board-8226.c
+++ arch/arm/mach-msm/board-8226.c
@@ -32,6 +32,7 @@
 #include <asm/mach/time.h>
 #include <mach/board.h>
 #include <mach/gpiomux.h>
+#include <mach/msm_kcal.h>
 #include <mach/msm_iomap.h>
 #include <mach/restart.h>
 #ifdef CONFIG_ION_MSM
@@ -127,6 +128,7 @@ void __init msm8226_add_drivers(void)
 		msm_clock_init(&msm8226_clock_init_data);
 	tsens_tm_init_driver();
 	msm_thermal_device_init();
+	msm_kcal_ctrl_init();
 }
 
 void __init msm8226_init(void)
